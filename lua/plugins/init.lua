90deae299d613828dfdde8820d6686fa5971c01f
Han Bui <ha@sparkenv.com>
2025-08-24 10:41:36 +0200

    bump

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -192,4 +192,13 @@ return {
     cmd = "TimerlyToggle",
     opts = {} -- optional
   },
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = "cd app && yarn install",
+    init = function()
+      vim.g.mkdp_filetypes = { "markdown" }
+    end,
+    ft = { "markdown" },
+  },
 }
