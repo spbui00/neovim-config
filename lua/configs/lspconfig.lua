b2e2f2938c840fdf5f0f45603c4479f755bee887
Han Bui <ha@sparkenv.com>
2026-03-23 12:28:36 +0100

    updated lspconfig

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -2,70 +2,60 @@ local on_attach = require("nvchad.configs.lspconfig").on_attach
 local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
-local lspconfig = require("lspconfig")
-
--- list of all servers configured.
-lspconfig.servers = {
-    "lua_ls",
-    "pyright",
-}
-
--- list of servers configured with default config.
 local default_servers = {
-  "clangd",          -- C/C++
-  "cssls",           -- CSS
-  "html",            -- HTML
-  "marksman",        -- Markdown
-  "ts_sl",        -- TypeScript/JavaScript
+  "clangd",
+  "cssls",
+  "html",
+  "marksman",
+  "ts_ls",
 }
 
--- lsps with default config
-for _, lsp in ipairs(default_servers) do
-    lspconfig[lsp].setup({
-        on_attach = on_attach,
-        on_init = on_init,
-        capabilities = capabilities,
-    })
+for _, server in ipairs(default_servers) do
+  vim.lsp.config(server, {
+    on_attach = on_attach,
+    on_init = on_init,
+    capabilities = capabilities,
+  })
+  vim.lsp.enable(server)
 end
 
-lspconfig.pyright.setup({
-    on_attach = on_attach,
-    on_init = on_init,
-    capabilities = capabilities,
-
-    settings = {
-        python = {
-            analysis = {
-                autoSearchPaths = true,
-                useLibraryCodeForTypes = true,
-                  typeCheckingMode = "basic", -- can be "off", "basic", or "strict"
-            },
-        },
+vim.lsp.config("pyright", {
+  on_attach = on_attach,
+  on_init = on_init,
+  capabilities = capabilities,
+  settings = {
+    python = {
+      analysis = {
+        autoSearchPaths = true,
+        useLibraryCodeForTypes = true,
+        typeCheckingMode = "basic",
+      },
     },
+  },
 })
+vim.lsp.enable("pyright")
 
-lspconfig.lua_ls.setup({
-    on_attach = on_attach,
-    on_init = on_init,
-    capabilities = capabilities,
-
-    settings = {
-        Lua = {
-            diagnostics = {
-                enable = false, -- Disable all diagnostics from lua_ls
-                -- globals = { "vim" },
-            },
-            workspace = {
-                library = {
-                    vim.fn.expand("$VIMRUNTIME/lua"),
-                    vim.fn.expand("$VIMRUNTIME/lua/vim/lsp"),
-                    vim.fn.stdpath("data") .. "/lazy/ui/nvchad_types",
-                    vim.fn.stdpath("data") .. "/lazy/lazy.nvim/lua/lazy",
-                    "${3rd}/love2d/library",
-                },
-                maxPreload = 100000,
-                preloadFileSize = 10000,
-            },
+vim.lsp.config("lua_ls", {
+  on_attach = on_attach,
+  on_init = on_init,
+  capabilities = capabilities,
+  settings = {
+    Lua = {
+      diagnostics = {
+        enable = false,
+      },
+      workspace = {
+        library = {
+          vim.fn.expand("$VIMRUNTIME/lua"),
+          vim.fn.expand("$VIMRUNTIME/lua/vim/lsp"),
+          vim.fn.stdpath("data") .. "/lazy/ui/nvchad_types",
+          vim.fn.stdpath("data") .. "/lazy/lazy.nvim/lua/lazy",
+          "${3rd}/love2d/library",
         },
+        maxPreload = 100000,
+        preloadFileSize = 10000,
+      },
     },
+  },
 })
+vim.lsp.enable("lua_ls")
