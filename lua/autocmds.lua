34072b346a6997486d0c034da80e54c5a3ad55bf
Han Bui <ha@sparkenv.com>
2026-07-18 17:20:25 +0200

    tab back to 4

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -3,8 +3,8 @@ require "nvchad.autocmds"
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "python",
   callback = function()
-    vim.opt_local.shiftwidth = 2
-    vim.opt_local.tabstop = 2
+    vim.opt_local.shiftwidth = 4
+    vim.opt_local.tabstop = 4
     vim.opt_local.expandtab = true
   end,
 })
